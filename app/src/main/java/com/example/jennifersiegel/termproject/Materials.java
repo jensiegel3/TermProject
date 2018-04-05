ec6ef17d501ac49b8e3cc9a2cf07a54c3347f92b
Anton Bagayev <bagayev_anto@bentley.edu>
2018-04-05 11:28:47 -0400

    Further Materials page updates

--- app/src/main/java/com/example/jennifersiegel/termproject/Materials.java
+++ app/src/main/java/com/example/jennifersiegel/termproject/Materials.java
@@ -4,54 +4,59 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class Materials extends Activity {
+public class Materials extends Activity implements AdapterView.OnItemSelectedListener {
     private Spinner materialTypeSpinner;
     private TextView materialTextView;
 
     private final String[] recyclableCategories = {
+            "<Select item material type>",
             "Paper & CardBoard",
             "Plastic",
             "Metal",
             "Glass"
     };
+    private final String itemListPrompt = "Recyclable items in this category are:\n\n";
+
     private final String paperItems =
-            "White & Colored Paper\n" +
-            "Shredded Paper\n" +
-            "Paper Plates & Bowls (clean)\n" +
-            "Wrapping Paper\n" +
-            "Newspaper\n" +
-            "Magazines & Catalogs\n" +
-            "Mail & Cards\n" +
-            "Folders\n" +
-            "Coffee Cups\n" +
-            "Cardboard Boxes\n" +
-            "Pizza Boxes (without grease stains)\n" +
-            "Milk/Cereal Containers\n" +
-            "Other Unstained Paper";
+            "- White & Colored Paper\n" +
+            "- Shredded Paper\n" +
+            "- Paper Plates & Bowls (clean)\n" +
+            "- Wrapping Paper\n" +
+            "- Newspaper\n" +
+            "- Magazines & Catalogs\n" +
+            "- Mail & Cards\n" +
+            "- Folders\n" +
+            "- Coffee Cups\n" +
+            "- Cardboard Boxes\n" +
+            "- Pizza Boxes (without grease stains)\n" +
+            "- Milk/Cereal Containers\n" +
+            "- Other Unstained Paper";
 
     private final String plasticItems =
-            "Plastic items (# 1-7)\n" +
-            "Coffee Lids\n" +
-            "Plastic Utensils\n" +
-            "Plastic Containers\n" +
-            "LaCava Clamshells\n" +
-            "Rigid Plastics";
+            "- Plastic items (# 1-7)\n" +
+            "- Coffee Lids\n" +
+            "- Plastic Utensils\n" +
+            "- Plastic Containers\n" +
+            "- LaCava Clamshells\n" +
+            "- Rigid Plastics";
 
 
     private final String metalItems =
-            "Aluminum\n" +
-            "Cans\n" +
-            "Tin\n" +
-            "Foil";
+            "- Aluminum\n" +
+            "- Cans\n" +
+            "- Tin\n" +
+            "- Foil";
 
     private final String glassItems =
-            "Beverage Bottles\n" +
-            "Food Jars\n" +
-            "Other glass containers";
+            "- Beverage Bottles\n" +
+            "- Food Jars\n" +
+            "- Other glass containers";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +64,7 @@ public class Materials extends Activity {
         setContentView(R.layout.activity_materials);
 
         materialTypeSpinner = (Spinner) findViewById(R.id.materialtypespinner);
+        materialTypeSpinner.setOnItemSelectedListener(this);   //set item click listener
         materialTextView = (TextView) findViewById(R.id.materialtextview);
 
         // Create an ArrayAdapter for material type spinner and connect it to the spinner
@@ -69,6 +75,29 @@ public class Materials extends Activity {
         recyclableCategoriesAdapter.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item);
         materialTypeSpinner.setAdapter(recyclableCategoriesAdapter);
+    }
+
+    // listener methods for spinner selection callbacks
+    public void onItemSelected(AdapterView<?> parent, View v, int position,
+                               long id) {
+        switch (position) {
+            case 1:
+                materialTextView.setText(itemListPrompt + paperItems);
+                break;
+            case 2:
+                materialTextView.setText(itemListPrompt + plasticItems);
+                break;
+            case 3:
+                materialTextView.setText(itemListPrompt + metalItems);
+                break;
+            case 4:
+                materialTextView.setText(itemListPrompt + glassItems);
+                break;
+        }
+
+    }
+    public void onNothingSelected(AdapterView<?> parent) {
+        materialTextView.setText("");
     }
 
     // create back options menu
