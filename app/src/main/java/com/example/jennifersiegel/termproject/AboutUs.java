8a816a12774ee54373b674055a6dcb37d3ccccf8
Miles Kaplow <kaplow_mile@bentley.edu>
2018-04-22 17:25:06 -0400

    Added Electronics Recycling

--- app/src/main/java/com/example/jennifersiegel/termproject/AboutUs.java
+++ app/src/main/java/com/example/jennifersiegel/termproject/AboutUs.java
@@ -29,8 +29,6 @@ public class AboutUs extends BaseActivity{
         send = (Button) findViewById(R.id.send);
         call1 = (Button) findViewById(R.id.call1);
         call2 = (Button) findViewById(R.id.call2);
-        subject = (EditText) findViewById(R.id.subject);
-        subject.setHint("Subject");
         text = (EditText) findViewById(R.id.text);
         text.setHint("What do you need help with?");
 
@@ -38,10 +36,9 @@ public class AboutUs extends BaseActivity{
         send.setOnClickListener(new OnClickListener() {
             public void onClick(View v){
 
-                Intent msg = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"));
-                msg.putExtra(Intent.EXTRA_EMAIL, "sustainability2@bentley.edu");
+                Intent msg = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:sustainability2@bentley.edu"));
+                msg.putExtra(Intent.EXTRA_SUBJECT, "Sustainability Question");
                 msg.putExtra(Intent.EXTRA_TEXT, text.getText());
-                msg.putExtra(Intent.EXTRA_SUBJECT, subject.getText());
 
                 //check to be sure email is installed on handset
                 if (msg.resolveActivity(getPackageManager()) != null) {
